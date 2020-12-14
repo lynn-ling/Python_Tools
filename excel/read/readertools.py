59809ba40533045cecd8e71be3ca3df611de68ac
ling.tong <m13898498790_1@163.com>
2020-12-14 16:12:13 +0800

    增加了求sum，avg，以及取最大值最小值的代码

--- excel/read/readertools.py
+++ excel/read/readertools.py
@@ -1,4 +1,5 @@
 import openpyxl
+from openpyxl.utils import get_column_letter, column_index_from_string
 
 class excel_reader():
     def __init__(self):
@@ -21,15 +22,80 @@ class file_reader():
         return self.workbook.get_sheet_names()
 
 
-    def sum_by_rows(self, sheetName, rows):
+    def sum_by_rows(self, sheetName, row):
         sheet = self.workbook.get_sheet_by_name(sheetName)
-       
+        sum = 0
+        row_num = sheet[row]
+        for cell in row_num:
+            if cell.value != None:
+                sum += float(cell.value)
+        return sum
+
 
 
     def sum_by_column(self,sheetName, column):
-        self.workbook.get_sheet_by_name(sheetName)
-        print(self.workbook.get_sheet_names())
-        print(column)
-        # sum return
-        return 0
+        sheet = self.workbook.get_sheet_by_name(sheetName)
+        sum = 0
+        column_num = sheet[column]
+        for cell in column_num:
+            if cell.value != None:
+                sum += float(cell.value)
+        return sum
+
+    def avg_by_column(self,sheetName,column):
+        sheet = self.workbook.get_sheet_by_name(sheetName)
+        sum = 0
+        count = 0
+        column_num = sheet[column]
+        for cell in column_num:
+            if cell.value != None:
+                sum += float(cell.value)  
+                count += 1      
+        return sum/count 
+    
+    def avg_by_row(self,sheetName,row):
+        sheet = self.workbook.get_sheet_by_name(sheetName)
+        sum = 0
+        count = 0
+        row_num = sheet[row]
+        for cell in row_num:
+            if cell.value != None:
+                sum += float(cell.value)  
+                count += 1      
+        return sum/count    
+
+    def max_by_column(self,sheetName,column):
+        sheet = self.workbook.get_sheet_by_name(sheetName)
+        column_num = sheet[column]
+        max = []
+        for cell in column_num:
+            if cell.value != None:
+                max.append(cell.value)
+    
+        for i in range(len(max)-1):
+            for j in range(len(max)-1-i):
+                if max[j]>max[j+1]:
+                    tmp = max[j]
+                    max[j] = max[j+1]
+                    max[j+1] = tmp
+        return max[-1]
+        
+    
+    def min_by_column(self,sheetName,column):
+        sheet = self.workbook.get_sheet_by_name(sheetName)
+        column_num = sheet[column]
+        min = []
+        for cell in column_num:
+            if cell.value != None:
+                min.append(cell.value)
+    
+        for i in range(len(min)-1):
+            for j in range(len(min)-1-i):
+                if min[j]<min[j+1]:
+                    tmp = min[j]
+                    min[j] = min[j+1]
+                    min[j+1] = tmp
+        return min[-1]
+
+
 
