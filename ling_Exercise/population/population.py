1d9aa5a3b0accbdcc530c32c398cde8d837a8876
ling.tong <m13898498790_1@163.com>
2020-12-16 17:38:38 +0800

    新加ling_Exercise

---
+++ ling_Exercise/population/population.py
@@ -0,0 +1,24 @@
+import openpyxl
+import pprint
+
+open_file = openpyxl.load_workbook("C://Users//baozipu//Desktop//automate_online-materials//censuspopdata.xlsx")
+sheet = open_file.get_sheet_by_name("Population by Census Tract")
+
+country_census = {}
+
+for cell in range(2, sheet.max_row+1):
+    state = sheet["B"+str(cell)].value
+    country = sheet["C"+str(cell)].value
+    pop = sheet["D"+str(cell)].value
+
+    country_census.setdefault(state,{})
+    country_census[state].setdefault(country,{"tracts":0,"pop":0})
+
+    country_census[state][country]["tracts"]+=1
+    country_census[state][country]["pop"]+=int(pop)
+
+result_file = open("population_result.py","w")
+result_file.write(pprint.pformat(country_census))
+result_file.close()
+
+
