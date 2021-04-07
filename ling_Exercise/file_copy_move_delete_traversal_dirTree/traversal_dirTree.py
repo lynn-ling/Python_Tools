7ee924e74e524578ac1e8c6f8471a740226d87cb
ling.tong <m13898498790_1@163.com>
2021-04-07 19:24:33 +0800

    修正

--- ling_Exercise/file_copy_move_delete_traversal_dirTree/traversal_dirTree.py
+++ ling_Exercise/file_copy_move_delete_traversal_dirTree/traversal_dirTree.py
@@ -3,9 +3,9 @@ import os
 #遍历目录树，将一个文件夹里所有的文件夹和文件都遍历一遍
 #os.walk在循环的每次迭代中，返回3个值：当前文件夹名称的字符串，当前文件夹中子文件夹的列表，当前文件夹中文件的列表
 #文件夹delicious，里面有↓
-#1，文件夹cats，内部有文件catnames.txt，zophie.jpg
-#2，文件夹walnut，内部有文件夹waffles(内部有文件夹butter.txt)
-#3，文件spam.txt
+#文件夹cats，内部有文件catnames.txt，zophie.jpg
+#文件夹walnut，内部有文件夹waffles(内部有文件夹butter.txt)
+#文件spam.txt
 
 for folderName,subfolders,filenames in os.walk('C:\\Users\\baozipu\\Desktop\\delicious'):
     print('The current folder is : '+folderName)
