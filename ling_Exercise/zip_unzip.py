b2c33efffa620b40d6115acac1bbf7b8f8f370a5
ling.tong <m13898498790_1@163.com>
2021-04-09 23:24:19 +0800

    修改

--- ling_Exercise/zip_unzip.py
+++ ling_Exercise/zip_unzip.py
@@ -38,7 +38,9 @@ zip_path("delicious",'C:\\Users\\baozipu\\Desktop','work.zip')
 os.chdir('C:\\Users\\baozipu\\Desktop\\')
 zip_read = zipfile.ZipFile('newzip.zip')
 print(zip_read.namelist())
+#namelist()方法返回ZIP文件中包含的所有文件和文件夹的字符串的列表
 info = zip_read.getinfo('delicious/cats/zophie.jpg')
+#getinfo()方法返回一个关于特定文件的ZipInfo对象，有表示字节数的file_size(原来文件大小)和compress_size(压缩后文件大小)属性
 print(info.file_size)
 
 #解压
@@ -47,6 +49,7 @@ os.chdir('C:\\Users\\baozipu\\Desktop')
 unzip_file = zipfile.ZipFile('newzip.zip')
 #extractall：解压该文件夹里所有的文件和文件夹，extract：解压某个文件或文件夹
 #如果括号里不写任何内容，那么会解压到当前目录下，即C:\\Users\\baozipu\\Desktop里
+#如果传递给extractall()方法的文件夹不存在，它会被创建
 unzip_file.extractall('C:\\Users\\baozipu\\Desktop\\delicious')
 unzip_file.close()
 
